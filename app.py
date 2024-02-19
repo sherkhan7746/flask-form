dcdda506318d3c82185a67487af82364598ec8c5
sherkhan <sherkhan1167746@gmail.com>
2024-02-20 01:10:13 +0500

    add get user data

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    print(request.method)
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+        print(first_name, last_name, email, date, occupation)
+
     return render_template("index.html")
 
 
